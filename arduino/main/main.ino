04b7e06cb7df331e35a479cefdda5241f5f53103
Richard Hayes <richardhayes777@gmail.com>
2016-06-12 12:46:56 +0100

    untested script to work with two sensors

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -5,33 +5,42 @@
 int ledPin = 13;                // choose the pin for the LED
 int inputPin1 = 2;               // choose the input pin (for PIR sensor)
 int inputPin2 = 3;
-int pirState = LOW;             // we start, assuming no motion detected
+int pirState1 = LOW;             // we start, assuming no motion detected
+int pirState2 = LOW;
 int val = 0;                    // variable for reading the pin status
  
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin1, INPUT);     // declare sensor as input
   pinMode(inputPin2, INPUT);     // declare sensor as input
  
   Serial.begin(9600);
 }
- 
-void loop(){
-  val = digitalRead(inputPin2);  // read input value
+
+int readPin(pin, state, title) {
+  val = digitalRead(pin);  // read input value
   if (val == HIGH) {            // check if the input is HIGH
-    digitalWrite(ledPin, HIGH);  // turn LED ON
-    if (pirState == LOW) {
+    digitalWrite(pin, HIGH);  // turn LED ON
+    if (state == LOW) {
       // we have just turned on
-      Serial.println("Motion detected!");
+      Serial.println(title + " on");
       // We only want to print on the output change, not state
-      pirState = HIGH;
+      state = HIGH;
     }
   } else {
-    digitalWrite(ledPin, LOW); // turn LED OFF
-    if (pirState == HIGH){
+    digitalWrite(pin, LOW); // turn LED OFF
+    if (state == HIGH){
       // we have just turned of
-      Serial.println("Motion ended!");
+      Serial.println(title + " off");
       // We only want to print on the output change, not state
-      pirState = LOW;
+      state = LOW;
     }
   }
+  return state;
+}
+ 
+void loop(){
+  pirState1 = readPin(inputPin1, pirState1, "First sensor")
+  pirState2 = readPin(inputPin2, pirState2, "Second sensor")
+  val = digitalRead(inputPin2);  // read input value
 }
