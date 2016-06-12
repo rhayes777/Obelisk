4aa54bd5a4a8168ab331a9577c430bf02ae835ab
Richard Hayes <richardhayes777@gmail.com>
2016-06-12 14:34:01 +0100

    parsing array

--- python/main.py
+++ python/main.py
@@ -1,14 +1,19 @@
 import serial
 import subprocess
 from os import system
+import ast
 
 # http://playground.arduino.cc/Interfacing/Python
 
 def loop():
     ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
     while True:
-        line = ser.readline()
+        line = ser.readline().strip()
         print line
+        arr = ast.literal_eval(line)
+        
+        for val in arr:
+            print val
 #         print type(line)
 #         if "First" in line:
 #             
