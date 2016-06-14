668131814d3d056653d863022d47cdafbe6385db
Richard Hayes <richardhayes777@gmail.com>
2016-06-14 23:20:33 +0100

    introduced odd behaviour but basically working

--- python/main.py
+++ python/main.py
@@ -4,14 +4,16 @@ import ast
 import audio_controller
 from operator import add
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
 # http://playground.arduino.cc/Interfacing/Python
 
-UPPER_LIMIT = 2000
+UPPER_LIMIT = 5000
 MIDDLE_LIMIT = 1000
 
-SAMPLE_SIZE = 10
+SAMPLE_SIZE = 1
+
+INPUT_ARRAY_SIZE = 3
 
 actions = {str([0, 0, 0]): audio_controller.ACRO_PAD_C,
            str([1, 0, 0]): audio_controller.WAVEDRIFT_PAD_C,
@@ -28,23 +30,33 @@ def loop():
     previous_result_array = None
     audio_controller.loop_next_wav_by_name(audio_controller.ACRO_PAD_C)
     ser = serial.Serial('/dev/cu.usbmodem1421', 9600)
+
+    current_sample = 0
+    sample_array = INPUT_ARRAY_SIZE * [0]
+
     while True:
         line = ser.readline().strip()
-        logging.debug(line)
         input_array = ast.literal_eval(line)
+#         logging.debug("input_array = {}".format(input_array))
 
-        current_sample = 0
-        sample_array = len(input_array) * [0]
-        while current_sample < SAMPLE_SIZE:
+        if current_sample < SAMPLE_SIZE:
             sample_array = map(add, sample_array, input_array)
             current_sample += 1
+            print sample_array
+        else:
 
-        average_array = map(lambda total: total / SAMPLE_SIZE, sample_array)
-        result_array = map(lambda result: 1 if result < UPPER_LIMIT else 0, average_array)
+            average_array = map(lambda total: total / SAMPLE_SIZE, sample_array)
 
-        if result_array != previous_result_array:
-            previous_result_array = result_array
-            audio_controller.loop_next_wav_by_name(actions[str(result_array)])
+            logging.debug("average_array = {}".format(average_array))
+
+            result_array = map(lambda result: 1 if result < UPPER_LIMIT else 0, average_array)
+
+            if result_array != previous_result_array:
+                previous_result_array = result_array
+                audio_controller.loop_next_wav_by_name(actions[str(result_array)])
+                
+            current_sample = 0
+            sample_array = INPUT_ARRAY_SIZE * [0]
 
 
 if __name__ == "__main__":
