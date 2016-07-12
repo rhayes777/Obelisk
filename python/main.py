6263ae200f80a526e7f0e85b2b794b6664a0a4d6
Richard <richardhayes777@gmail.com>
2016-07-12 19:08:48 +0100

    normalising

--- python/main.py
+++ python/main.py
@@ -9,7 +9,7 @@ import util
 
 # http://playground.arduino.cc/Interfacing/Python
 
-MAX_DISTANCE = 200
+MAX_DISTANCE = 300
 CLOSE_DISTANCE = 100
 TIME_DISTANCE_CONVERSION_FACTOR = 58.138
 
@@ -17,57 +17,97 @@ SAMPLE_SIZE = 100
 
 INPUT_ARRAY_SIZE = 2
 
+NORMALISING_THRESHOLD = 0.1
+
+max_distance_array = INPUT_ARRAY_SIZE * [MAX_DISTANCE]
+
 sample_arrays = []
 for n in range(0, SAMPLE_SIZE):
     sample_arrays.append(INPUT_ARRAY_SIZE * [MAX_DISTANCE])
-    
+
 print sample_arrays
 
-           
 audio_samples = [
-                    audio_controller.A_FAR_Master,
-                    audio_controller.A_NEAR_Master,
-                    audio_controller.B_FAR_Master,
-                    audio_controller.B_NEAR_Master,
-                    audio_controller.C_FAR_Master,
-                    audio_controller.C_NEAR_Master,
-                    audio_controller.D_FAR_Master,
-                    audio_controller.D_NEAR_Master
-                    ]
+    audio_controller.A_FAR_Master,
+    audio_controller.A_NEAR_Master,
+    audio_controller.B_FAR_Master,
+    audio_controller.B_NEAR_Master,
+    audio_controller.C_FAR_Master,
+    audio_controller.C_NEAR_Master,
+    audio_controller.D_FAR_Master,
+    audio_controller.D_NEAR_Master
+]
+
+last_sample_array = None
+should_normalise = True
+
+
+def miliseconds_to_meters(value):
+    return value / TIME_DISTANCE_CONVERSION_FACTOR
+
+
+def miliseconds_to_meters_array(array):
+    return map(lambda value: miliseconds_to_meters(value), array)
+
+
+def normalise(new_sample_array):
+    global last_sample_array
+    global should_normalise
+    global max_distance_array
+
+    if last_sample_array is not None:
+        test_array = [abs(new_sample - last_sample) < NORMALISING_THRESHOLD for new_sample, last_sample in
+                      zip(new_sample_array, last_sample_array)]
+        print test_array
+        if not False in test_array:
+            for n in range(0, len(new_sample_array)):
+                if new_sample_array[n] < MAX_DISTANCE:
+                    max_distance_array[n] = new_sample_array[n]
+
+            should_normalise = False
+    print "last sample being set to {} with type {}".format(new_sample_array, type(new_sample_array))
+    last_sample_array = new_sample_array
+    print "normalised"
 
 
 def loop():
-    previous_result_array = INPUT_ARRAY_SIZE * [0]
     ser = serial.Serial(util.get_arduino_port(), 9600)
-    
+
     for n in range(0, 2 * INPUT_ARRAY_SIZE):
         print "Playing {}".format(audio_samples[n])
         audio_controller.loop_wav_on_new_thread(audio_samples[n])
 
     while True:
         line = ser.readline().strip()
-        input_array = ast.literal_eval(line)
+        input_array = miliseconds_to_meters_array(ast.literal_eval(line))
         print input_array
-        
+
+        if should_normalise:
+            print "normalising"
+            normalise(input_array)
+            print max_distance_array
+            continue
+
         sample_arrays.pop(0)
         sample_arrays.append(input_array)
 
         average_array = INPUT_ARRAY_SIZE * [0]
-        
+
         for sample in sample_arrays:
             average_array = map(add, average_array, sample)
-            
-        average_array = map(lambda value: value / (SAMPLE_SIZE * TIME_DISTANCE_CONVERSION_FACTOR) , average_array)
+
+        average_array = map(lambda value: value / SAMPLE_SIZE, average_array)
 
         logging.debug("average_array = {}".format(average_array))
 
         for n in range(0, INPUT_ARRAY_SIZE):
+            max_distance = max_distance_array[n]
             distance = average_array[n]
             if distance < 0:
                 distance = 0
-            if distance > MAX_DISTANCE:
-                distance = MAX_DISTANCE
-            volume_far = 1 - distance / MAX_DISTANCE
+            if distance > max_distance:
+                distance = max_distance
+            volume_far = 1 - distance / max_distance
             volume_near = 0
             if distance < CLOSE_DISTANCE:
                 volume_near = 1 - distance / CLOSE_DISTANCE
@@ -75,9 +115,8 @@ def loop():
             audio_controller.queues[2 * n].put(volume_near)
             audio_controller.queues[2 * n + 1].put(volume_far)
 
-             
-            
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     loop()
-#     audio_controller.loop_wav_on_new_thread("A_FAR_Master_converted.wav")
+# audio_controller.loop_wav_on_new_thread("A_FAR_Master_converted.wav")
