c52b5c252340d164246569f697c843c7cf04f0a3
Richard <richardhayes777@gmail.com>
2016-08-07 21:32:52 +0100

    moved timeout decorator to util

--- python/util.py
+++ python/util.py
@@ -1,6 +1,9 @@
 import sys
 import glob
 import serial
+from threading import Thread
+import functools
+
 
 def serial_ports():
     """ Lists serial port names
@@ -43,6 +46,33 @@ def get_arduino_ports():
         print "No ports found! Is arduino plugged in?"
         assert False
     return tty_ports
+    
+
+def timeout(timeout):
+    def deco(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            res = [Exception('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, timeout))]
+            def newFunc():
+                try:
+                    res[0] = func(*args, **kwargs)
+                except Exception, e:
+                    res[0] = e
+            t = Thread(target=newFunc)
+            t.daemon = True
+            try:
+                t.start()
+                t.join(timeout)
+            except Exception, je:
+                print 'error starting thread'
+                raise je
+            ret = res[0]
+            if isinstance(ret, BaseException):
+                raise ret
+            return ret
+        return wrapper
+    return deco
+    
     
 if __name__=="__main__":
     print get_arduino_port()
