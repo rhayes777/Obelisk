5c3b653c24923209bcb54a73e2702267f4a67ab2
Richard <richardhayes777@gmail.com>
2016-08-08 12:29:23 +0100

    simple script to download samples from dropbox

---
+++ python/download_samples.py
@@ -0,0 +1,16 @@
+import urllib2
+
+url = "https://www.dropbox.com/sh/gmpxk8f1qv7ht1u/AADFY0c6hUwP8x-iyWm-QzUga?dl=1"
+filename = "../samples.zip"
+
+def download_samples():
+    u = urllib2.urlopen(url)
+    data = u.read()
+    u.close()
+     
+    with open([filename], "wb") as f:
+        f.write(data)
+    
+    
+if __name__ == "__main__":
+    download_samples()
