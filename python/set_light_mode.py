8ccec81e80494081a1a70fefccd89171a6abca7a
Richard <richardhayes777@gmail.com>
2016-08-19 14:03:14 +0100

    set_light_mode has ON and OFF constants

--- python/set_light_mode.py
+++ python/set_light_mode.py
@@ -2,9 +2,13 @@ import sys
 import Arduino
 
 
+ON = 1
+OFF = 2
+
 arduinos = Arduino.get_all()
 
-def set_light_mode(light_mode="1"):
+def set_light_mode(light_mode=ON):
+    light_mode = str(light_mode)
     for arduino in arduinos:
         arduino.set_light_modes([light_mode, light_mode])
     
