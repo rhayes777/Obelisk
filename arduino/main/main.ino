23ade403cb82fea019adb1e28bb0b963c6be5409
Richard <richardhayes777@gmail.com>
2016-08-23 17:13:10 +0100

    thought out light patterns

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -9,16 +9,16 @@ int lightPins[] = {10, 11};
 int lightModes[] = {0, 0};
 boolean isLightOnArray[] = {false, false};
 
-int lightPattern0[] = {0, 0, 0, 0, 0, 0, 0, 0};
-int lightPattern1[] = {1, 1, 1, 1, 1, 1, 1, 1};
-int lightPattern2[] = {0, 1, 1, 1, 1, 1, 1, 1};
-int lightPattern3[] = {1, 1, 1, 0, 1, 1, 0, 0};
-int lightPattern4[] = {1, 0, 1, 0, 0, 1, 0, 0};
-int lightPattern5[] = {0, 1, 0, 1, 1, 0, 1, 1};
-int lightPattern6[] = {1, 1, 1, 1, 0, 0, 0, 0};
-int lightPattern7[] = {1, 1, 0, 1, 1, 0, 1, 0};
-int lightPattern8[] = {1, 1, 0, 0, 1, 1, 0, 0};
-int lightPattern9[] = {1, 0, 1, 0, 1, 0, 1, 0};
+int lightPattern0[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int lightPattern1[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int lightPattern2[] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int lightPattern3[] = {0, 1, 1, 1, 1, 0, 1, 1, 1, 1};
+int lightPattern4[] = {1, 0, 1, 0, 1, 0, 1, 1, 1, 1};
+int lightPattern5[] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 0};
+int lightPattern6[] = {1, 0, 1, 1, 0, 1, 1, 1, 0, 0};
+int lightPattern7[] = {1, 1, 1, 1, 1, 0, 0, 0, 0, 0};
+int lightPattern8[] = {1, 1, 0, 1, 1, 0, 1, 1, 0, 0};
+int lightPattern9[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
 
 int* lightPatterns[10];
 
